d9e5d47cb49518417e0d5cdb2ccca0430957c4a6
Kevin Tieu <pttieu@uh.edu>
2023-10-10 14:56:30 -0500

    add basic info

--- solvent-calculator.py
+++ solvent-calculator.py
@@ -1,4 +1,3 @@
-import pandas as pd
 # A test volume is 3ml --> 3000 ul
 ELECTROLYTE_VOL = 3000
 
@@ -10,24 +9,22 @@ ADDITIVE_LIST = ['TMP','TEP','DMMP','TTFEP','FEMC','FDEC','BTFE','TTE','HFPN','P
 ADDITIVE_COUNT_TOTAL = len(ADDITIVE_LIST)
 ADDITIVE_RATIO_TOTAL = [0.05, 0.1, 0.15]
 
-column_header = ['LP30'] + ADDITIVE_LIST
-df = pd.DataFrame([],columns=['LP30'] + ADDITIVE_LIST)
-list_add = [0:1:length(column_header)]
 # helper function
-# final_comp = []
-# def VolumeCal(current_comp, curr_add):
-#     if curr_add == ADDITIVE_COUNT_TOTAL:
-#         return
+
+def VolumeCal(final_comp, current_comp, curr_add):
+    if curr_add == ADDITIVE_COUNT_TOTAL:
+        return final_comp
+    
+
+
 
 
 
 
 def main():
-    additive_ratio = ADDITIVE_RATIO_TOTAL[0]
-    # additive_volume = VolumeCal([],[],0)
-    # for i in range(0,ADDITIVE_COUNT_TOTAL):
-        # print("Additive {}: {} mL".format(ADDITIVE_LIST[i],sum(additive_volume[:][i])))
-    print("no error")
+    additive_volume = VolumeCal([],[],0)
+    for i in range(0,ADDITIVE_COUNT_TOTAL):
+        print("Additive {}: {} mL".format(ADDITIVE_LIST[i],sum(additive_volume[:][i])))
 
 if __name__ == '__main__':
     main()
