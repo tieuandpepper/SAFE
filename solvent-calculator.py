93cad1f4d13094f89cb0923a7dc3e01e39c81eb5
Kevin Tieu <pttieu@uh.edu>
2023-10-22 23:20:21 -0500

    create list of chemical compositions and calculate volume quantity of solvents and electrolyte

--- solvent-calculator.py
+++ solvent-calculator.py
@@ -1,10 +1,11 @@
+import csv
 # A test volume is 3ml --> 3000 ul
 TOTAL_VOL = 3000
 
 # Relative ratio of adjustment of additives 
 ADDITIVE_INTERVAL = 0.1
 # Maximum additive concentration in the electrolyte mixture
-ADDITIVE_MAX_CON = 0.2
+ADDITIVE_MAX_CON = 0.15
 ADDITIVE_LIST = ['TMP','TEP','DMMP','TTFEP','FEMC','FDEC','BTFE','TTE','HFPN','PFPN','TFPN','FPPN']
 ADDITIVE_COUNT_TOTAL = len(ADDITIVE_LIST)
 ADDITIVE_RATIO_TOTAL = [0.05, 0.1, 0.15]
@@ -18,10 +19,35 @@ final_comp = []
 test_comp = []
 # helper function
 
-def VolumeCal(final_comp, current_comp, curr_add):
-    if curr_add == ADDITIVE_COUNT_TOTAL:
-        return final_comp
+def PermutationFinder(permutation_arr, arr, idx, target, balance):
+    # if combination is found
+    if (balance == 0):
+        permutation_arr.append(arr[:])
+        for i in range(idx-1,len(arr)):
+            if (idx < 0): break
+            arr[i] = 0
+        # print(result[-1])
+        # print(arr)
+        return
     
+    # base condition
+    if (balance < 0):
+        # print('<0')
+        return
+    if (idx == len(arr)):
+        # print('out of bound')
+        return
+
+    # loop start from prev number at array idx-1
+    for k in range(0,balance+1):
+        # next element of array is k
+        arr[idx] = k
+        # print("current condition: {}, idx {}, balance {}, k = {}".format(arr,idx,balance-k,k))
+
+        # call recursively with balance in mind
+        PermutationFinder(permutation_arr, arr, idx+1, target, balance - k)
+
+
 
 def ScreenTest():
     for idx, additive in enumerate(ADDITIVE_LIST):
@@ -49,14 +75,56 @@ def GetTotalVol(comp_list, have_LP30 = True):
     
     return total_vol
 
+# test for duplicates
+def Validation(arr):
+    arr.sort()
+    for i in range(1,len(arr)):
+        if (arr[i] == arr[i-1]):
+            return False
+    return True
 
 
 def main():
-    ScreenTest()
-    print(test_comp)
-    total_vol = GetTotalVol(test_comp)
-    print(total_vol)
+    arr = [0]*ADDITIVE_COUNT_TOTAL
+    permutation_arr = []
+    PermutationFinder(permutation_arr,arr, 0, 10, 10)
+    permutation_arr.sort()
+    print("Total permuation:", len(permutation_arr))
+    print("Validation: {}".format(Validation(permutation_arr)))
+    total_vol = [0]*(ADDITIVE_COUNT_TOTAL)
+    for i in range(len(permutation_arr)):
+        for j in range(ADDITIVE_COUNT_TOTAL):
+            total_vol[j] += permutation_arr[i][j]
+    # print(permutation_arr)
     
+    # The data is written as ratio 0-10 (0=0%, 10=100%)
+    file_name = "SAFE/additive_composition.csv"
+    with open(file_name,'w',newline='') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(ADDITIVE_LIST)
+        csv_writer.writerows(permutation_arr)
+    
+    # the data is written with [ul] as the unit (1ul = 10^-6 l)
+    file_name = "SAFE/additve_volume.csv"
+    with open(file_name,'w',newline='') as file:
+        csv_writer = csv.writer(file,lineterminator='\n')
+        csv_writer.writerow(ADDITIVE_LIST)
+        csv_writer.writerow(total_vol)
+    
+    txt_file = "SAFE/chemicals_report.txt"
+    with open(txt_file,'w') as file:
+        line = "Total combinations: {}\n".format(len(permutation_arr))
+        file.write(line)
+        line = "Total formulations: {}\n".format(len(permutation_arr)*len(ADDITIVE_RATIO_TOTAL))
+        file.write(line)
+        line = "LP30 = {:>16,.2f} mL\n".format(TOTAL_VOL*len(permutation_arr)*(len(ADDITIVE_RATIO_TOTAL)-sum(ADDITIVE_RATIO_TOTAL))/1000)
+        file.write(line)
+        for i in range(ADDITIVE_COUNT_TOTAL):
+            # additive volume percentage: additive_total_volume / 10
+            # total volume for each additive = 3[electrolyte mL] * [additive volume percentage] * (0.05 + 0.1 + 0.15)
+            line = "{:<5} = {:>16,.2f} mL\n".format(ADDITIVE_LIST[i],TOTAL_VOL*total_vol[i]*(sum(ADDITIVE_RATIO_TOTAL))/10/1000)
+            file.write(line)
+    print('complete')
 
 if __name__ == '__main__':
     main()
