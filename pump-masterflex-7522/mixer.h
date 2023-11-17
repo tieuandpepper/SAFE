66881bf7ebd41107b43eb1065a6e1787de4d07c2
Yao Lab <yaolab@YaoLab-Macbook-Air.local>
2023-11-17 14:27:07 -0600

    add driver for mixer

---
+++ pump-masterflex-7522/mixer.h
@@ -0,0 +1,18 @@
+#ifndef Mixer_h
+#define Mixer_h
+
+#include "Arduino.h"
+
+#define DELAY_MS 5
+
+typedef struct mixer_type
+{
+  bool running = false;
+  uint8_t pin;
+} mixer_t;
+
+bool MixerStart(mixer_t* mixer);
+bool MixerStop(mixer_t* mixer);
+bool MixerRun(mixer_t * mixer, uint32_t time_ms);
+
+#endif
