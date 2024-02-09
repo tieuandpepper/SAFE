942656befe40e2ff836385f5861e302ec7c03dd3
Kevin Tieu <pttieu@uh.edu>
2024-02-09 13:53:58 -0600

    analyzing the temperature data

--- data_procesor/data_graphing.py
+++ data_procesor/data_graphing.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-df = pd.read_fwf('temp_data.txt')
+df = pd.read_fwf('data_procesor/temp_data.txt')
+df.drop(df.index[0:200], inplace=True)
 # print(df)
 df["time"] = range(0,len(df["temperature"]))
 df["time"] = [x / 2 for x in df["time"]]
@@ -16,6 +17,17 @@ for (index,value) in enumerate(temp_np):
         temp_np[index] = 0
 
 df["np_gradient"] = temp_np
+
+df = df.assign(fire=0)
+fire_ignited = False
+for i in df.index:
+    if df['np_gradient'][i] > 2:
+        fire_ignited = True
+    if fire_ignited and df['np_gradient'][i] < -2:
+        fire_ignited = False
+    if fire_ignited:
+        df['fire'][i] = 1
+
 
 print(df)
 df.to_csv("output.csv")
@@ -37,10 +49,11 @@ ax1.tick_params(axis='y', labelcolor=color)
 
 ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 
-color = 'tab:red'
+color = 'tab:orange'
 ax2.set_ylabel('gradient degC/s', color=color)  # we already handled the x-label with ax1
 ax2.plot(df["time"],df["np_gradient"], color=color, linestyle = ':')
 ax2.tick_params(axis='y', labelcolor=color)
+ax2.plot(df["time"],df['fire'], color='red', linewidth = 1.3)
 
 fig.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.grid(which='both',axis='both',linestyle = '--')
