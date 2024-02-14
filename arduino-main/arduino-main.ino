44a189071320eee98b80dc779628b8048f5874f4
Phat Kevin Tieu <phattieuthien@gmail.com>
2024-02-14 17:38:10 -0600

    create pinout.h file to store the mapping of Arduino inputs/outputs

--- arduino-main/arduino-main.ino
+++ arduino-main/arduino-main.ino
@@ -1,95 +1,28 @@
-/*
- * PUMP CONTROL MASTERFLEX 7522
- * Arduino UNO to DB25
- * DB25 pinout:
- * DB25_P1 : Speed control INPUT voltage 0-10V
- * DB25_P3 : Speed control INPUT GROUND 
- * DB25_P5 : Speed signal OUTPUT GROUND
- * DB25_P14: Speed signal OUTPUT voltage 0-10V
- * DB25_P15: Remote START/STOP INPUT
- * DB25_P16: Remote CW/CCW INPUT
- * DB25_P17: Remote START/STOP, CW/CCW, Prime GROUND
- * DB25_P20: Remote prime INPUT
- *  
- *                     +-----------------------+
- *                     |    Wiring diagram     |
- *                     |          DB25         |
- *                     +-----------------------+
- *      _____________________________________________________
- *      [ 13  12  11  10  09  08  07  06  05  04  03  02  01 ]
- *      [                                                    ]
- *       \  25  24  23  22  21  20  19  18  17  16  15  14  / 
- *        \________________________________________________/
- *  
- *                     +-----------------------+  
- *                     |    Wiring diagram     |
- *                     |      Arduino UNO      |
- *                     +-----------------------+ 
- *                         _________________
- *                         |           SCL-|-
- *                         |           SDA-|-
- *                         |          AREF-|-
- *                         |           GND-|- 
- *                         |           D13-|- 
- *                        -|-IOREF     D12-|- 
- *                        -|-RESET     D11-|- 
- *                        -|-3.3V      D10-|- 
- *        mixer_relay_Vcc -|-5V        D09-|- 
- *                        -|-GND       D08-|-
- *                        -|-GND           | 
- *                        -|-VIN       D07-|- 
- *                         |           D06-|-  mixer_relay_in
- *                        -|-A0        D05-|- DB25_P01
- *                        -|-A1        D04-|- DB25_P15
- *                        -|-A2        D03-|- DB25_P16
- *               DB25_P14 -|-A3        D02-|- DB25_P20
- *                        -|-A4         TX-|-
- *                        -|-A5         RX-|-
- *                         |_______________|
+/**
+ * @file arduino-main.ino
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-02-14
  * 
+ * @copyright Copyright (c) 2024
  * 
- *                     +-----------------------+
- *                     | PINOUT & POWER SUPPLY |
- *                     |       & GROUND        |
- *                     +-----------------------+
- *                 
- *      COMPONENT        |             PIN             | ARDUINO PIN | POWER SUPPLY 
- *      -----------------+-----------------------------+-------------+--------------
- *      MIXING PUMP      |    CONNECTOR-DB25 PIN-01    |    D05      |              
- *      MIXING PUMP      |    CONNECTOR-DB25 PIN-03    |    GND      |              
- *      MIXING PUMP      |    CONNECTOR-DB25 PIN-05    |    GND      |              
- *      MIXING PUMP      |    CONNECTOR-DB25 PIN-14    |    A3       |              
- *      MIXING PUMP      |    CONNECTOR-DB25 PIN-15    |    D04      |              
- *      MIXING PUMP      |    CONNECTOR-DB25 PIN-16    |    D03      |              
- *      MIXING PUMP      |    CONNECTOR-DB25 PIN-17    |    GND      |              
- *      MIXING PUMP      |    CONNECTOR-DB25 PIN-20    |    D02      |              
- *      MIXER            |          RELAY - GND        |    GND      |              
- *      MIXER            |          RELAY - VCC        |     5V      |          
- *      MIXER            |          RELAY - IN         |    D06      |              
-*/
+ */
 
 #include "src/PumpMasterflex.h"
 #include "Controller.h"
 #include "src/mixer.h"
 #include <DFRobot_MAX31855.h>
-
-#define PUMP_SPEED_CONTROL       5
-#define PUMP_REMOTE_CONTROL      4
-#define PUMP_CLOCKWISE_CONTROL   3
-#define PUMP_PRIME_CONTROL       2
-#define PUMP_SPEED_FEEDBACK      A3 // analog pin
-
-#define MIXER_PIN                  6
-
+#include "arduino_pinout.h"
 /*----------------------------------------------------------------------------------------------------------*/
 
 
 MasterflexDB25Interface_t mixing_pump_interface {
-  .start_stop_pin  = PUMP_REMOTE_CONTROL,
-  .direction_pin   = PUMP_CLOCKWISE_CONTROL,
-  .prime_pin       = PUMP_PRIME_CONTROL,
-  .voltage_in_pin  = PUMP_SPEED_CONTROL,
-  .voltage_out_pin = PUMP_SPEED_FEEDBACK,
+  .start_stop_pin  = PIN_MIXING_PUMP_REMOTE_CONTROL,
+  .direction_pin   = PIN_MIXING_PUMP_CLOCKWISE_CONTROL,
+  .prime_pin       = PIN_MIXING_PUMP_PRIME_CONTROL,
+  .voltage_in_pin  = PIN_MIXING_PUMP_SPEED_CONTROL,
+  .voltage_out_pin = PIN_MIXING_PUMP_SPEED_FEEDBACK,
 };
 
 PumpMasterflex mixing_pump = PumpMasterflex(mixing_pump_interface);
@@ -100,7 +33,7 @@ DFRobot_MAX31855 max31855;
 /// @brief Setup/ Initialization. Run first and run ONCE
 void setup() {
   Serial.begin(9600);
-  mixer.pin = MIXER_PIN;
+  mixer.pin = PIN_MIXER_ENABLE;
   pinMode(mixer.pin,OUTPUT);
   digitalWrite(mixer.pin, LOW);
   mixing_pump.Connect();
---
+++ arduino-main/arduino_pinout.h
@@ -0,0 +1,118 @@
+/*
+ * PUMP CONTROL MASTERFLEX 7522
+ * Arduino UNO to DB25
+ * DB25 pinout:
+ * DB25_P1 : Speed control INPUT voltage 0-10V
+ * DB25_P3 : Speed control INPUT GROUND 
+ * DB25_P5 : Speed signal OUTPUT GROUND
+ * DB25_P14: Speed signal OUTPUT voltage 0-10V
+ * DB25_P15: Remote START/STOP INPUT
+ * DB25_P16: Remote CW/CCW INPUT
+ * DB25_P17: Remote START/STOP, CW/CCW, Prime GROUND
+ * DB25_P20: Remote prime INPUT
+ *  
+ *                     +-----------------------+
+ *                     |    Wiring diagram     |
+ *                     |          DB25         |
+ *                     +-----------------------+
+ *      _____________________________________________________
+ *      [ 13  12  11  10  09  08  07  06  05  04  03  02  01 ]
+ *      [                                                    ]
+ *       \  25  24  23  22  21  20  19  18  17  16  15  14  / 
+ *        \________________________________________________/
+ *  
+ *                     +-----------------------+  
+ *                     |    Wiring diagram     |
+ *                     |      Arduino UNO      |
+ *                     +-----------------------+ 
+ *                         _________________
+ *                         |           SCL-|- temp sensor SCL
+ *                         |           SDA-|- temp sensor SDA
+ *                         |          AREF-|-
+ *                         |           GND-|- 
+ *                         |           D13-|- 
+ *                        -|-IOREF     D12-|- 
+ *                        -|-RESET     D11-|- 
+ *                        -|-3.3V      D10-|- rotary valve TX
+ *        mixer_relay_Vcc -|-5V        D09-|- rotary valve RX
+ *                        -|-GND       D08-|- mixer relay enable
+ *                        -|-GND           | 
+ *                        -|-VIN       D07-|- lighter enable pin
+ *                         |           D06-|- lighter initialization pin
+ *                        -|-A0        D05-|- DB25_P01
+ *                        -|-A1        D04-|- DB25_P15
+ *                        -|-A2        D03-|- DB25_P16
+ *               DB25_P14 -|-A3        D02-|- DB25_P20
+ *                        -|-A4         TX-|-
+ *                        -|-A5         RX-|-
+ *                         |_______________|
+ * 
+ * 
+ * 
+ *                     +-----------------------+  
+ *                     |    Wiring diagram     |
+ *                     |        Lighter        |
+ *                     +-----------------------+ 
+ *                       __________________________________________
+ *                       |   ______________         __________    |
+ *                       |   |            |         |  pMOS  |    |
+ *                       |   |            |         | Source-|----+    
+ *                       +---|---button---|---------|-Drain  |     
+ *                           |            |         |  Gate  |
+ *                           |            |         |____|___|
+ *                           |            |              |
+ *         Arduino GND ------| - initial +|----+     Arduino D07
+ *                           |   ization  |    |
+ *                           |            |  Arduino D06
+ *                           |  VCC   GND |
+ *                           |___|_____|__|
+ *                               |     |
+ *                            5V Power supply
+ * 
+ *                     +-----------------------+
+ *                     | PINOUT & POWER SUPPLY |
+ *                     |       & GROUND        |
+ *                     +-----------------------+
+ *                 
+ *      COMPONENT        |             PIN             | ARDUINO PIN | POWER SUPPLY 
+ *      -----------------+-----------------------------+-------------+--------------
+ *      MIXING PUMP      |    CONNECTOR-DB25 PIN-01    |    D05      |              
+ *      MIXING PUMP      |    CONNECTOR-DB25 PIN-03    |    GND      |              
+ *      MIXING PUMP      |    CONNECTOR-DB25 PIN-05    |    GND      |              
+ *      MIXING PUMP      |    CONNECTOR-DB25 PIN-14    |    A3       |              
+ *      MIXING PUMP      |    CONNECTOR-DB25 PIN-15    |    D04      |              
+ *      MIXING PUMP      |    CONNECTOR-DB25 PIN-16    |    D03      |              
+ *      MIXING PUMP      |    CONNECTOR-DB25 PIN-17    |    GND      |              
+ *      MIXING PUMP      |    CONNECTOR-DB25 PIN-20    |    D02      |              
+ *      MIXER            |          RELAY - GND        |    GND      |              
+ *      MIXER            |          RELAY - VCC        |     5V      |          
+ *      MIXER            |          RELAY - IN         |    D08      |              
+ *      LIGHTER          |    ENABLE - ON/OFF BUTTON   |    D07      |               
+ *      LIGHTER          |         INITIALIZATION      |    D06      |
+ *      LIGHTER          |       INITIALIZATION_GND    |    GND      |      
+ *      LIGHTER          |          POWER_VCC          |             |      3.7-5 V
+ *      LIGHTER          |          POWER_GND          |             |       GND
+ *      TEMP SENSOR      |             VCC             |     5V      |
+ *      TEMP SENSOR      |             GND             |     GND     |
+ *      TEMP SENSOR      |          SCL Clock          |     SCL     |
+ *      TEMP SENSOR      |          SDA Data           |     SDA     |
+ *      ROTARY VALVE     |           RS232 RX          |     D09     |
+ *      ROTARY VALVE     |           RS232 TX          |     D10     |
+ *      ROTARY VALVE     |           RS232 GND         |     GND     |
+ *      ROTARY VALVE     |           DC 24V +          |             |       24V
+ *      ROTARY VALVE     |           DC 24V -          |             |       GND
+ * 
+ *      
+ *
+ *  
+ */
+
+// Masterflex mixing pump pinout
+#define PIN_MIXING_PUMP_SPEED_CONTROL       5
+#define PIN_MIXING_PUMP_REMOTE_CONTROL      4
+#define PIN_MIXING_PUMP_CLOCKWISE_CONTROL   3
+#define PIN_MIXING_PUMP_PRIME_CONTROL       2
+#define PIN_MIXING_PUMP_SPEED_FEEDBACK      A3 // analog pin
+
+// Mixer pin
+#define PIN_MIXER_ENABLE                    6
