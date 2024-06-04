c6494386234bfc4caf6bb430afa5bb2a0eb8f89d
Phat Kevin Tieu <phattieuthien@gmail.com>
2024-06-04 16:06:18 -0500

    update pinout

--- arduino-main/arduino_pinout.h
+++ arduino-main/arduino_pinout.h
@@ -2,14 +2,14 @@
  * PUMP CONTROL MASTERFLEX 7522
  * Arduino UNO to DB25
  * DB25 pinout:
- * DB25_P1 : Speed control INPUT voltage 0-10V
- * DB25_P3 : Speed control INPUT GROUND 
- * DB25_P5 : Speed signal OUTPUT GROUND
- * DB25_P14: Speed signal OUTPUT voltage 0-10V
- * DB25_P15: Remote START/STOP INPUT
- * DB25_P16: Remote CW/CCW INPUT
- * DB25_P17: Remote START/STOP, CW/CCW, Prime GROUND
- * DB25_P20: Remote prime INPUT
+ * DB25_P1 : Speed control INPUT voltage 0-10V (YELLOW)
+ * DB25_P3 : Speed control INPUT GROUND (RED)
+ * DB25_P5 : Speed signal OUTPUT GROUND (ORANGE)
+ * DB25_P14: Speed signal OUTPUT voltage 0-10V (GREEN)
+ * DB25_P15: Remote START/STOP INPUT (PURPLE)
+ * DB25_P16: Remote CW/CCW INPUT (BLUE)
+ * DB25_P17: Remote START/STOP, CW/CCW, Prime GROUND (GRAY)
+ * DB25_P20: Remote prime INPUT (BROWN)
  *  
  *                     +-----------------------+
  *                     |    Wiring diagram     |
@@ -42,7 +42,7 @@
  *                        -|-A0        D05-|- mixing_pump DB25_P01
  *                        -|-A1        D04-|- mixing_pump DB25_P15
  *                        -|-A2        D03-|- mixing_pump DB25_P16
- *   mixing_pump DB25_P14 -|-A3        D02-|- mixing_pump DB25_P20
+ *   mixing_pump DB25_P14 -|-A3        D02-|- 
  *      transfer_pump SDA -|-A4         TX-|-
  *      transfer_pump SCL -|-A5         RX-|-
  *                         |_______________|
@@ -56,14 +56,13 @@
  *                 
  *      COMPONENT        |             PIN             | ARDUINO PIN | POWER SUPPLY 
  *      -----------------+-----------------------------+-------------+--------------
- *      MIXING PUMP      |    CONNECTOR-DB25 PIN-01    |    D05      |              
- *      MIXING PUMP      |    CONNECTOR-DB25 PIN-03    |    GND      |              
- *      MIXING PUMP      |    CONNECTOR-DB25 PIN-05    |    GND      |              
- *      MIXING PUMP      |    CONNECTOR-DB25 PIN-14    |    A3       |              
- *      MIXING PUMP      |    CONNECTOR-DB25 PIN-15    |    D04      |              
- *      MIXING PUMP      |    CONNECTOR-DB25 PIN-16    |    D03      |              
- *      MIXING PUMP      |    CONNECTOR-DB25 PIN-17    |    GND      |              
- *      MIXING PUMP      |    CONNECTOR-DB25 PIN-20    |    D02      |              
+ *      MIXING PUMP      |    DB25 PIN-01 YELLOW       |    D05      |              
+ *      MIXING PUMP      |    DB25 PIN-03 RED          |    GND      |              
+ *      MIXING PUMP      |    DB25 PIN-05 ORANGE       |    GND      |              
+ *      MIXING PUMP      |    DB25 PIN-14 GREEN        |    A3       |              
+ *      MIXING PUMP      |    DB25 PIN-15 PURPLE       |    D04      |              
+ *      MIXING PUMP      |    DB25 PIN-16 BLUE         |    D03      |              
+ *      MIXING PUMP      |    DB25 PIN-17 GRAY         |    GND      |              
  *      MIXER            |          RELAY - GND        |    GND      |              
  *      MIXER            |          RELAY - VCC        |     5V      |          
  *      MIXER            |          RELAY - IN         |    D10      |              
@@ -95,7 +94,6 @@
 #define PIN_MIXING_PUMP_SPEED_CONTROL       5
 #define PIN_MIXING_PUMP_REMOTE_CONTROL      4
 #define PIN_MIXING_PUMP_CLOCKWISE_CONTROL   3
-#define PIN_MIXING_PUMP_PRIME_CONTROL       2
 #define PIN_MIXING_PUMP_SPEED_FEEDBACK      A3 // analog pin
 
 // Mixer pin
@@ -112,3 +110,4 @@
 // I2C Address
 #define I2C_ADDR_TEMP_SENSOR                0x10
 #define I2C_ADDR_TRANSFER_PUMP              0x67
+#define I2C_ADDR_UART_ARDUINO               0xAA
